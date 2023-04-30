5130b015abf43184f9faa7f2d4f03eb2a89d3bda
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2023-04-30 21:13:51 +0700

    fix message starting service

--- googleservice/src/index.ts
+++ googleservice/src/index.ts
@@ -4,7 +4,7 @@ const start = async () => {
   console.log("Starting google service...");
 
   app.listen(3000, async () => {
-    console.log("Google service listening on port 3000!!!!");
+    console.log("Google service listening on port 3000!!!");
   });
 };
 
