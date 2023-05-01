51ab38f8b2a0d4372c50b1de4c5b1def1e2f8b47
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2023-05-01 19:08:10 +0700

    update service

--- facebookservice/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { json } from "body-parser";
-
-const app = express();
-app.use(json());
-
-app.listen(3000, () => {
-	console.log("Facebook service listening on port 3000!!!");
-});
